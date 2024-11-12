f26e908b8d2c23b2011f0cf608360a37c2fdf700
Lucas Natario <lucasenatario@gmail.com>
2024-11-12 16:31:17 -0300

    Add remaining reports

--- Com2900G10/Reportes/SPReportes.sql
+++ Com2900G10/Reportes/SPReportes.sql
@@ -77,3 +77,177 @@ AS
              END
     FOR XML RAW, ELEMENTS, ROOT('XML')
 END
+
+
+GO
+CREATE OR ALTER PROCEDURE reportes.productos_vendidas_por_rango
+	@desde datetime, @hasta datetime
+AS
+BEGIN
+	IF @desde is null OR @hasta is null
+	BEGIN
+		RAISERROR('Debe ingresar los limites de fechas', 10, 1);
+	END
+
+    -- Empieza la consulta
+	SELECT * 
+	FROM  (
+		SELECT
+			p.nombre_producto as producto,
+			SUM( df.cantidad) as cantidad
+		FROM venta.detalle_factura df
+			INNER JOIN venta.factura f ON f.id_factura = df.id_factura AND f.pagada = 1
+			INNER JOIN producto.producto p ON p.id_producto = df.id_producto
+		GROUP BY p.nombre_producto
+	) t1
+	ORDER BY t1.cantidad DESC
+    FOR XML RAW, ELEMENTS, ROOT('XML')
+END;
+-- EXEC reportes.productos_vendidas_por_rango '2019-01-01', '2019-12-31'
+GO
+
+
+CREATE OR ALTER PROCEDURE reportes.productos_vendidas_por_rango_por_sucursal
+	@desde datetime, @hasta datetime
+AS
+BEGIN
+	IF @desde is null OR @hasta is null
+	BEGIN
+		RAISERROR('Debe ingresar los limites de fechas', 10, 1);
+	END
+
+    -- Empieza la consulta
+	SELECT * 
+	FROM  (
+		SELECT
+			s.id_sucursal,
+			s.reemplazar_por as nombre_sucursal,
+			p.nombre_producto as producto,
+			SUM( df.cantidad) as cantidad
+		FROM venta.detalle_factura df
+			INNER JOIN venta.factura f ON f.id_factura = df.id_factura AND f.pagada = 1
+			INNER JOIN sucursal.sucursal s ON s.id_sucursal = f.id_sucursal
+			INNER JOIN producto.producto p ON p.id_producto = df.id_producto
+		GROUP BY s.id_sucursal, s.reemplazar_por, nombre_producto
+	) t1
+	ORDER BY t1.cantidad DESC
+    FOR XML RAW, ELEMENTS, ROOT('XML')
+END;
+-- EXEC reportes.productos_vendidas_por_rango_por_sucursal '2019-01-01', '2019-12-31'
+GO
+
+
+CREATE OR ALTER PROCEDURE reportes.top5_productos_vendidos_mes_por_semana
+	@mes tinyint, @anio smallint
+AS
+BEGIN
+	IF @mes is null OR @anio is null
+	BEGIN
+		RAISERROR('Debe ingresar el mes a consultar', 10, 1);
+	END;
+
+    -- Empieza la consulta
+	WITH CTE(id_producto, nombre_producto, cantidad, semana, posicion) AS
+	(
+		SELECT 
+			t1.id_producto, 
+			t1.nombre_producto,
+			t1.cantidad,
+			t1.semana,
+			ROW_NUMBER() OVER (PARTITION BY DATEPART(WEEK, t1.semana) ORDER by t1.cantidad)
+		FROM (
+			SELECT
+				p.id_producto,
+				p.nombre_producto,
+				DATEPART(WEEK, f.fechaHora) AS semana,
+				SUM(df.cantidad) AS cantidad
+			FROM venta.detalle_factura df
+				INNER JOIN venta.factura f ON 
+					f.id_factura = df.id_factura AND 
+					f.pagada = 1 AND
+					MONTH(f.fechaHora) = @mes AND
+					YEAR(f.fechaHora) = @anio
+				INNER JOIN producto.producto p ON p.id_producto = df.id_producto
+			GROUP BY p.id_producto, p.nombre_producto, DATEPART(WEEK, f.fechaHora)
+		) AS t1
+	)
+	SELECT 
+		id_producto,
+		nombre_producto,
+		cantidad,
+		semana - DATEPART(WEEK, DATEFROMPARTS(@anio, @mes, 1)) + 1 AS SemanaDelMes
+	FROM CTE
+	WHERE posicion <= 5
+    FOR XML RAW, ELEMENTS, ROOT('XML')
+END;
+-- EXEC reportes.top5_productos_vendidos_mes_por_semana 2, 2019
+GO
+
+CREATE OR ALTER PROCEDURE reportes.top5_menos_productos_vendidos_mes
+	@mes tinyint, @anio smallint
+AS
+BEGIN
+	IF @mes is null OR @anio is null
+	BEGIN
+		RAISERROR('Debe ingresar el mes a consultar', 10, 1);
+	END;
+
+    -- Empieza la consulta
+	WITH CTE(id_producto, nombre_producto, cantidad, posicion) AS
+	(
+			SELECT
+				p.id_producto,
+				p.nombre_producto,
+				SUM(df.cantidad) AS cantidad,
+				ROW_NUMBER() OVER (ORDER by p.id_producto)
+			FROM venta.detalle_factura df
+				INNER JOIN venta.factura f ON 
+					f.id_factura = df.id_factura AND 
+					f.pagada = 1 AND
+					MONTH(f.fechaHora) = @mes AND
+					YEAR(f.fechaHora) = @anio
+				INNER JOIN producto.producto p ON p.id_producto = df.id_producto
+			GROUP BY p.id_producto, p.nombre_producto
+	)
+	SELECT 
+		id_producto,
+		nombre_producto,
+		cantidad
+	FROM CTE
+	WHERE posicion <= 5
+    FOR XML RAW, ELEMENTS, ROOT('XML')
+END;
+-- EXEC reportes.top5_menos_productos_vendidos_mes 3, 2019
+GO
+
+
+CREATE OR ALTER PROCEDURE reportes.acumulado_ventas_por_fecha_por_sucursal
+	@fecha date, @id_sucursal smallint
+AS
+BEGIN
+	IF @fecha is null OR @id_sucursal is null
+	BEGIN
+		RAISERROR('Debe ingresar la fecha y sucursal a consultar', 10, 1);
+	END;
+
+    -- Empieza la consulta
+	WITH CTE(id_factura, total, fecha_hora, acumulado) AS
+	(
+			SELECT
+				f.id_factura,
+				f.total,
+				f.fechaHora,
+				SUM(f.total) OVER(ORDER BY f.id_factura)
+			FROM  venta.factura f
+			WHERE
+				f.pagada = 1 AND
+				CAST(f.fechaHora AS date) = @fecha AND
+				f.id_sucursal = @id_sucursal
+	)
+	SELECT 
+		*
+	FROM CTE
+    FOR XML RAW, ELEMENTS, ROOT('XML')
+END;
+-- EXEC reportes.acumulado_ventas_por_fecha_por_sucursal '2019-03-01', 1
+GO
